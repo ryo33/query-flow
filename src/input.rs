f9b28f8606cd47c4a8b9dca809d2946d96d1beec
Ryo Hirayama <ryo12redstone@gmail.com>
2023-02-09 11:52:08 +0900

    Update

--- src/input.rs
+++ src/input.rs
@@ -1,25 +1,7 @@
-use std::{any::Any, convert::Infallible, fmt::Debug, hash::Hash};
+use std::{any::Any, fmt::Debug, hash::Hash};
 
-use crate::{context::Ctx, error::QueryResult, output::QueryOutput, query::Query};
+use crate::output::QueryOutput;
 
 pub trait Input: Debug + Hash + Any + PartialEq + Eq + Send + Sync + 'static {
     type Output: QueryOutput;
-}
-
-impl<O, I> Query for I
-where
-    O: QueryOutput,
-    I: Input<Output = O>,
-{
-    type Output = Option<O>;
-    type Error = Infallible;
-
-    fn run(&self, ctx: &mut Ctx) -> QueryResult<Self> {
-        // The default value
-        Ok(None)
-    }
-
-    fn on_input(&self) {
-        // Do nothing
-    }
 }
